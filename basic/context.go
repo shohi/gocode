6503ed93dab98a6177969134ea720f366f8a7329
shohi <oshohi@gmail.com>
2017-10-22 23:22:15 +0800

    update

--- basic/context.go
+++ basic/context.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"golang.org/x/net/context"
 )
 
 func main() {
-	ctx, doCancelFunc := context.WithCancel(context.WithValue(context.Background(), "key", "value"))
+	keyStr := "key"
+	valueCtx := context.WithValue(context.Background(), &keyStr, "value")
+	dlCtx, dlCancelFunc := context.WithTimeout(valueCtx, 10*time.Second)
+	defer dlCancelFunc()
+	ctx, doCancelFunc := context.WithCancel(dlCtx)
 	defer doCancelFunc()
 
 	fmt.Println(ctx)
