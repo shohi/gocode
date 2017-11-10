32a6e7a904ec59c72ab68b288ea21aa6923f41ab
shohi <oshohi@gmail.com>
2017-11-10 17:09:27 +0800

    add more url test

--- basic/url_test.go
+++ basic/url_test.go
@@ -6,6 +6,25 @@ import (
 	"testing"
 )
 
-func TestUrl(t *testing.T) {
+func TestUrlPathEscape(t *testing.T) {
 	fmt.Println(url.PathEscape("lang:>50"))
+}
+
+func TestUrlParse(t *testing.T) {
+	urlStr := "http://localhost:9090/hello/newyorker?season=summer"
+	fmt.Println(url.Parse(urlStr))
+}
+
+func TestUrlResolveReference(t *testing.T) {
+	urlStr := "http://localhost:9090/hello/newyorker?season=summer"
+	base, _ := url.Parse(urlStr)
+
+	fmt.Println(base)
+
+	urlStr1 := "a/b/c/d.ts"
+	url1, err := url.Parse(urlStr1)
+	fmt.Println(err)
+
+	fmt.Println(base.ResolveReference(url1))
+
 }
