4c378a7f689466193a5848787a56613135b21ff1
shohi <oshohi@gmail.com>
2017-11-23 17:32:28 +0800

    add more test on uint32

--- basic/int_test.go
+++ basic/int_test.go
@@ -2,6 +2,7 @@ package basic
 
 import (
 	"log"
+	"math"
 	"testing"
 )
 
@@ -9,4 +10,13 @@ func TestUintConvert(t *testing.T) {
 	var a int
 	a = -1
 	log.Println(uint32(a))
+}
+
+func TestUint32(t *testing.T) {
+	a := uint32(10)
+	var b uint32
+	b = math.MaxUint32
+
+	log.Println(a, b+uint32(8))
+	log.Println(a > (b + uint32(8)))
 }
