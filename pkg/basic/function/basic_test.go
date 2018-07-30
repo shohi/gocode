7f3d57a72b03b91ae3fff17cbbd5434557a18abc
shohi <oshohi@gmail.com>
2018-07-30 14:22:44 +0800

    add nil func test

--- pkg/basic/function/basic_test.go
+++ pkg/basic/function/basic_test.go
@@ -13,3 +13,12 @@ func TestFunction(t *testing.T) {
 	h := func() {}
 	log.Printf("%p ==> %p", Hello, h)
 }
+
+func TestNilFunction(t *testing.T) {
+	var f func()
+	if f != nil {
+		f()
+	} else {
+		t.Logf("Nil function")
+	}
+}
