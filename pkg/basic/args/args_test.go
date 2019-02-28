b80a2c4ec0ed4b5199d11a0145daf656687f375a
shohi <oshohi@gmail.com>
2019-02-28 11:10:51 +0800

    add tests for variadic func

---
+++ pkg/basic/args/args_test.go
@@ -0,0 +1,39 @@
+package args_test
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+type options struct {
+	manualAck bool
+	timeout   time.Duration
+}
+
+type Option func(opts *options) error
+
+func WithTimeout(d time.Duration) Option {
+	return func(opts *options) error {
+		opts.timeout = d
+		return nil
+	}
+}
+
+func runWithOptions(option ...Option) {
+	opts := &options{}
+
+	for _, o := range option {
+		o(opts)
+	}
+
+	log.Printf("options: %v", opts)
+}
+
+func TestArgs_VariableArguments(t *testing.T) {
+	// without options
+	runWithOptions()
+
+	// with options
+	runWithOptions(WithTimeout(10 * time.Millisecond))
+}
